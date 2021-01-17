94448f9126168b948201711999cd1ce80713fc23
eshval <77581871+eshval@users.noreply.github.com>
2021-01-17 22:31:53 +0530

    Update discord_webhook.py

--- discord_webhook.py
+++ discord_webhook.py
@@ -2,7 +2,7 @@ from discord_webhooks import DiscordWebhooks
 
 #Put your discord webhook url here.
 
-webhook_url = ''
+webhook_url = 'https://discord.com/channels/@me'
 
 
 def send_msg(class_name,status,start_time,end_time):
